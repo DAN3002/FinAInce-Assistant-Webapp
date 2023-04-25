c403b2bb6829ce6541b23c56501b6fb1c454f88e
DAN3002 <dinhanh300229@gmail.com>
2023-04-25 18:15:00 +0700

    Disable upload ava when signup

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,26 +1,28 @@
 import React, { useState } from 'react';
-import Add from '../img/addAvatar.png';
+// import Add from '../img/addAvatar.png';
 import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
-import { auth, db, storage } from '../firebase';
-import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
+import { auth, db } from '../firebase';
+// import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
 import { doc, setDoc } from 'firebase/firestore';
 import { useNavigate, Link } from 'react-router-dom';
 import bankingAPI from '../api/banking';
 
+const DEFAULT_AVA_LINK = 'https://firebasestorage.googleapis.com/v0/b/junctionx-web-app.appspot.com/o/user-avatar.png?alt=media&token=53d689d8-96e5-419b-8ea9-39196e33ab6e';
+
 const Register = () => {
 	const [err, setErr] = useState(false);
-	const [loading, setLoading] = useState(false);
+	// const [loading, setLoading] = useState(false);
 	const navigate = useNavigate();
 
 	const handleSubmit = async (e) => {
-		setLoading(true);
+		// setLoading(true);
 		e.preventDefault();
 		const email = e.target[0].value;
 		const displayName = e.target[1].value;
 		const fullName = e.target[2].value;
 		const phoneNumber = e.target[3].value;
 		const password = e.target[4].value;
-		const file = e.target[5].files[0];
+		// const file = e.target[5].files[0];
 
 		try {
 			//Create user
@@ -38,40 +40,28 @@ const Register = () => {
 				password: password,
 			};
 
-			bankingAPI.createUser(userData);
-			//Create a unique image name
-			const date = new Date().getTime();
-			const storageRef = ref(storage, `${displayName + date}`);
+			await bankingAPI.createUser(userData);
 
-			await uploadBytesResumable(storageRef, file).then(() => {
-				getDownloadURL(storageRef).then(async (downloadURL) => {
-					try {
-						//Update profile
-						await updateProfile(res.user, {
-							displayName,
-							photoURL: downloadURL,
-						});
-						//create user on firestore
-						await setDoc(doc(db, 'users', res.user.uid), {
-							uid: res.user.uid,
-							displayName,
-							email,
-							photoURL: downloadURL,
-						});
-
-						//create empty user chats on firestore
-						await setDoc(doc(db, 'userChats', res.user.uid), {});
-						navigate("/");
-					} catch (err) {
-						console.log(err);
-						setErr(true);
-						setLoading(false);
-					}
-				});
+			// Update profile picture with fixed path
+			await updateProfile(res.user, {
+				displayName,
+				photoURL: DEFAULT_AVA_LINK,
 			});
+
+			//create user on firestore
+			await setDoc(doc(db, 'users', res.user.uid), {
+				uid: res.user.uid,
+				displayName,
+				email,
+				photoURL: DEFAULT_AVA_LINK,
+			});
+
+			//create empty user chats on firestore
+			await setDoc(doc(db, 'userChats', res.user.uid), {});
+			navigate("/");
 		} catch (err) {
 			setErr(true);
-			setLoading(false);
+			// setLoading(false);
 		}
 	};
 
@@ -93,7 +83,8 @@ const Register = () => {
 					/>
 					<input required type='password' placeholder='Password' />
 
-					<input
+					<button>Sign up</button>
+					{/* <input
 						required
 						style={{ display: 'none' }}
 						type='file'
@@ -103,8 +94,7 @@ const Register = () => {
 						<img src={Add} alt='' />
 						<span>Add an avatar</span>
 					</label>
-					<button disabled={loading}>Sign up</button>
-					{loading && 'Uploading and compressing the image please wait...'}
+					{loading && 'Uploading and compressing the image please wait...'} */}
 					{err && <span>Something went wrong</span>}
 				</form>
 				<p>
