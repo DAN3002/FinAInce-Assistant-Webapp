6042b7d27d8f282d4fd5dd9dfcacd465cf744e94
DAN3002 <dinhanh300229@gmail.com>
2023-04-26 09:43:00 +0700

    ADD: create group chat

---
+++ src/utils/CreateGroup.js
@@ -0,0 +1,23 @@
+import ChatRooms from "../models/ChatRooms";
+import {db} from "../firebase";
+import {collection, getDocs} from "firebase/firestore";
+
+const createGroup = async (usernames) => {
+    const members = [];
+    const ref = collection(db, "users");
+    const docs = await getDocs(ref);
+    docs.forEach((doc) => {
+        if (usernames.includes(doc.data().username)) {
+            members.push({
+                uid: doc.id,
+                username: doc.data().username,
+            });
+        }
+    });
+    const chatRoomRef = ChatRooms.newChatRoom(members, false);
+    return chatRoomRef.then((docRef) => {
+        return docRef.id;
+    });
+};
+
+export default createGroup;
