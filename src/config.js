c43fd5c53f7f3a6cf35c3ff50879c71add0e8768
DAN3002 <dinhanh300229@gmail.com>
2023-05-07 23:04:36 +0700

    Fix image

--- src/config.js
+++ src/config.js
@@ -2,10 +2,10 @@ export const BOT_DATA = {
 	uid: "zKUtGYyWqRTIQa5J9SqqMfDZy8t2",
 	username: "Chat Bot",
 	email: "chatbot@gmail.com",
-	photoURL: "https://firebasestorage.googleapis.com/v0/b/junctionx-web-app.appspot.com/o/chat-bot-ava.webp?alt=media&token=ff990914-5935-4226-959a-e2e755c63c72",
+	photoURL: "./chat-bot-ava.webp",
 }
 
-export const USER_AVA = 'https://firebasestorage.googleapis.com/v0/b/junctionx-web-app.appspot.com/o/user-avatar.png?alt=media&token=53d689d8-96e5-419b-8ea9-39196e33ab6e';
+export const USER_AVA = './user-avatar.png';
 
 export const NLP_HISTORY_LIMIT = -1;
 export const NLP_SERVER = 'https://ai-bhdl.jsclub.me';
